1a169b2a7af0d5f06fee67e941b55bc256deee9b
Semih KEŞKEK <keskeksmh@gmail.com>
2018-09-19 23:19:44 +0300

    [Interfaces] corrected interfaces

--- src/app/Interfaces/Model/ToDone.d.ts
+++ src/app/Interfaces/Model/ToDone.d.ts
@@ -1,6 +1,6 @@
 export interface IToDoneProperty {
 	userId: number;
-    id: number;
+    id?: number; // id is not required
     title: string;
     completed: boolean
 }
--- src/app/Interfaces/Store.d.ts
+++ src/app/Interfaces/Store.d.ts
@@ -1,9 +1,10 @@
 //#region Local Imports
-
+import { IList, IToDone } from '@App/Interfaces';
 //#endregion Local Imports
 
 export interface IStore {
 	'@Store': {
-
+		List: IList.IState,
+		ToDone: IToDone.IState
 	}
 }
